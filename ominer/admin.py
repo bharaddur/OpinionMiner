1544d4b0af6d97ab5c1d0bb1bd6be58a27fd4b09
bharaddur <mcangeyik@gmail.com>
2022-11-30 23:55:11 +0300

    created initial db model to keep tweets.

--- ominer/admin.py
+++ ominer/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+
 # Register your models here.
+
+from .models import TweetQuery, Tweets
+
+admin.site.register(TweetQuery)
+admin.site.register(Tweets)
+
--- ominer/models.py
+++ ominer/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import  User, Group
+from django.conf import settings
 
 # Create your models here.
+
+class TweetQuery(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    query = models.TextField(max_length=255)
+
+    def __str__(self):
+         return self.query
+
+class Tweets(models.Model):
+    
+    query = models.ForeignKey(TweetQuery,default=None, on_delete=models.CASCADE)
+    tweet = models.TextField(max_length=280)
+
+    def __str__(self):
+         return self.tweet
