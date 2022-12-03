089fe66f959f9cafd1aa57237028fba050b672b3
bharaddur <mcangeyik@gmail.com>
2022-12-03 20:01:35 +0300

    Forms.py addition and some improvements in user login logout

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from . forms import UserRegisterForm
 # Create your views here.
 
 def home(request):
@@ -10,14 +11,14 @@ def home(request):
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Hi {username}, your account was created successfully')
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
 
     return render(request,'users/register.html', {'form': form})
 
