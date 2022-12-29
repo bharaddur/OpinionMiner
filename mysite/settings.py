144fe5081006c95cdd440c6c23cf262313f83fd6
bharaddur <mcangeyik@gmail.com>
2022-12-30 00:50:07 +0300

    Database config for deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 ##DEBUG = True
 
 #deployment
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG", "True")
 
 ##ALLOWED_HOSTS = ['127.0.0.1']
 
